24697ee6ea3617c624bd622e70e940366b8e438d
Chris <cjjeakle@umich.edu>
2014-02-27 05:18:10 -0500

    sockets are up and running

--- server.js
+++ server.js
@@ -5,14 +5,16 @@
 
 
 var express = require('express');
+var http = require('http');
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
 var logfmt = require('logfmt');
 var routes = require('./routes');
-var http = require('http');
 var path = require('path');
 var pg = require('pg').native;
 var request = require('request');
-
-var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -25,13 +27,14 @@ app.use(express.cookieParser());
 // Secret will be made an env var later
 app.use(express.cookieSession({secret: 'SECRET!'}));
 app.use(app.router);
+app.use(express.static(__dirname + '/static'));
 
 // Only serve content from the /public/... directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Listen on the environment's desired port or at 5000
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+server.listen(port, function() {
 	console.log('Listening on ' + port);
 });
 
@@ -46,15 +49,21 @@ app.configure('development', function(){
   app.locals.pretty = true;
 });
 
-// Create the server
-http.createServer(app);
-
 
 //////////////////////////////// Implementation ////////////////////////////////
 
 
+
 // How to handle get requests
 app.get('/', routes.nouncaptcha(pg));
+
+// Socket handlers
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
 
 
 
