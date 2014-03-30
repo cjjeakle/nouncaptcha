0b1847487f4eb1d96e52672ae2772ba9b5bd45f8
Chris <cjjeakle@umich.edu>
2014-03-29 20:15:12 -0400

    Updated analysis queries to be more insightful.

--- db_stuff/analysis.sql
+++ db_stuff/analysis.sql
@@ -1,12 +1,16 @@
-SELECT count(*) from game_log;
 
+-- Excluded users never formally disconnected due to error
+-- Appears to be due to Heroku Dyno idleing during their game
+\echo 'List of connected UUIDs'
 SELECT l1.log_id, l1.uuid FROM game_log l1 
 LEFT JOIN game_log l2
 ON l1.uuid = l2.uuid AND l2.event like '%disconnect%'
 WHERE l1.event = 'new game' AND l2.event IS NULL
+AND l1.log_id != 356 AND l1.log_id != 1233 AND l1.log_id != 1219
 ORDER BY l1.log_id DESC;
 
-SELECT t.token, s.response_id, s.country, count(*)
+\echo 'List of most recent 5 survey responses, their token and image count'
+SELECT t.token, s.how_found, count(*)
 FROM game_log l 
 INNER JOIN game_survey s 
 ON l.uuid = s.uuid 
@@ -14,14 +18,25 @@ INNER JOIN game_tokens t
 ON t.uuid = l.uuid 
 WHERE l.event = 'starting'
 GROUP BY t.token, s.response_id 
-ORDER BY s.time DESC;
+ORDER BY s.time DESC
+LIMIT 10;
 
+\echo 'Number of survey responses'
+select count(*) - 1 as count from game_survey;
+
+/*
+\echo 'List of taboo tags and their corresponding image'
 SELECT i.url, t.noun, t.count 
 FROM images i 
 INNER JOIN tags t 
 ON i.img_id = t.img_id 
 WHERE t.count >= 5
-ORDER BY t.count DESC;
+ORDER BY t.img_id DESC;
+*/
 
+\echo 'Total number of taboo tags'
+SELECT count(*) from tags
+where count >= 5;
+
+\echo 'Total number of tags'
 SELECT count(*) from tags;
-
