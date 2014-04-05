93a6b76c411f879909a742afaaa7e3cddd6bc6f4
Chris <cjjeakle@umich.edu>
2014-04-05 17:38:10 -0400

    Updated captcha survey for usability and clearness

--- cap_handlers.js
+++ cap_handlers.js
@@ -207,7 +207,7 @@ function send_prompt(socket) {
 					var percentage = socket.cap_count / min_for_approval * 60;
 					if(socket.cap_count > min_for_approval) {
 						percentage = 50;
-						percentage += socket.cap_count / max_attempts * 50; 
+						percentage += (socket.cap_count - min_for_approval) / max_attempts * 50; 
 					}
 
 					// Remove cap_answers from below in prod, only added to make survey easier
